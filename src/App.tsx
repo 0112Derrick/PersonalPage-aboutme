04c69efd0099d667649e50eb95697a775f51e436
0112Derrick <0112derrick@gmail.com>
2024-02-12 16:01:22 -0600

    u

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavMenu from "./NavMenu";
 import { Profile } from "./Profile";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import { Col, Container, ListGroupItem, Row } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 import { Home } from "./Home";
 import { Services } from "./Services";
 import { Portfolio } from "./Portfolio";
